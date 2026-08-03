2b467c6971d0ad69837f24ade99b134941b3b9fd
Sawano <aldianokto@gmail.com>
2026-08-03 15:25:54 +0700

    Add copyright notice to eterbit.go

--- eterbit.go
+++ eterbit.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by the Apache License.
 // that can be found in the root directory of this repository.
 // Project: Eterbit / Blockchain Core
+
 package main
 
 import (
